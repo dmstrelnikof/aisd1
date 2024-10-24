da6fdd49cb4a825db8ad98eca38903ea840f67cc
dmitrijstrelnikov <dimastrelnikov2305@gmail.com>
2024-10-24 19:16:01 +0300

    надеюсь все

--- MATLab/theory.m
+++ MATLab/theory.m
@@ -1,23 +1,28 @@
 % Определение функций
-T_best = @(n) n .* (log(n).^2);
-
+T_best = @(n) (n .* (n-1)) / 2;
+T_avg = @(n) (n .* (n-1)) / 4;
+T_worst = @(n) (n-1) / 2;
 
 % Значения n
 n_values = 0:100000;
 
 % Вычисление значений функций
 T_best_values = T_best(n_values);
-
+T_avg_values = T_avg(n_values);
+T_worst_values = T_worst(n_values);
 
 % Построение графиков
 figure;
 hold on;
 
 % Лучший случай
-plot(n_values, T_best_values, 'g', 'DisplayName', '');
+plot(n_values, T_best_values, 'g', 'DisplayName', 'Лучший случай');
 
+% Средний случай
+plot(n_values, T_avg_values, 'b', 'DisplayName', 'Средний случай');
 
-
+% Худший случай
+plot(n_values, T_worst_values, 'r', 'DisplayName', 'Худший случай');
 
 % Настройки графика
 title('Графики функций сложности T(n)');
